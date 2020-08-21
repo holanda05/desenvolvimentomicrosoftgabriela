d1178dfe8133c0a593da07fb5cd536e90321bea9
Gabriela de Holanda <gabrieladeholanda05@gmail.com>
2020-08-20 21:45:30 -0300

    Adição do menu

--- Vendascon/Program.cs
+++ Vendascon/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿ using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Vendascon
 {
@@ -6,7 +7,35 @@ namespace Vendascon
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int opcao;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(" --- PROJETO DE VENDAS ---\n");
+                Console.WriteLine("1 - Cadastrar cliente");
+                Console.WriteLine("2 - Listar  clientes");
+                Console.WriteLine("0 - Sair \n");
+                Console.WriteLine("Escolher uma opção:");
+                opcao = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine(" --- CADASTRAR CLIENTE ---\n");
+                        break;
+                    case 2:
+                        Console.WriteLine(" --- LISTAR CLIENTE ---\n");
+                        break;
+                    case 0:
+                        Console.WriteLine("Saindo...\n");
+                        break;
+                    default:
+                        Console.WriteLine(" --- OPÇÃO INVÁLIDA!!! ---\n");
+                        break;
+                }
+                Console.WriteLine("\nAperte uma tecla para continuar...");
+                Console.ReadKey();
+            } while (opcao != 0);
         }
     }
 }
